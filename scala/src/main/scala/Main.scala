84b500f2982d53a96fde4d346c96310fb971811d
Anton Van Moere <anton.vanmoere@gmail.com>
2017-08-12 21:58:08 +0200

    added data for A-Z

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -84,9 +84,10 @@ object Main extends JFXApp {
     text = "save"
   }
 
-  var alphabet = List("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+  var alphabet = List("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z").
+    map(x => x.toUpperCase)
   var counter = 0
-  val latexDescriptor = "a_to_z"
+  val latexDescriptor = "A_to_Z"
 
   val imageView = new ImageView()
   imageView.image = updateTex(alphabet(counter).toString)
